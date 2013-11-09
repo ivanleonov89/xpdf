e80f36f604231f2aa8dae45b67f3405fc027c85b
ivan <ivanleonov89@gmail.com>
2013-11-09 18:13:20 +0200

    Adds ignoring errors to to_formatted_text

--- lib/xpdf.rb
+++ lib/xpdf.rb
@@ -3,7 +3,8 @@ require "xpdf/version"
 module Xpdf
   class << self
     def to_formatted_text(filename, target = nil)
-      run_command(:pdftotext, "-layout -enc UTF-8 #{filename} #{target || '-'}", !!target)
+      flags_line = "-layout -enc UTF-8 -q #{filename} #{target || '-'}"
+      run_command(:pdftotext, flags_line, !!target)
     end
 
     def run_command(command, args, reroute_errors = true)
